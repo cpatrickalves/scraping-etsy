32cb8fc51d166dcbe6d08ac3ae8df1767d24c091
Patrick Alves <cpatrickalves@gmail.com>
2019-01-21 08:13:57 -0300

    Spider with a Search String

--- etsy/settings.py
+++ etsy/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'etsy.spiders'
 #USER_AGENT = 'etsy (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- etsy/spiders/etsy_search.py
+++ etsy/spiders/etsy_search.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+#==============================================================================
+#title           :etsy_search.py
+#description     :A spider to scrape etsy.com products based on a search string.
+#author          :Patrick Alves (cpatrickalves@gmail.com)
+#date            :21-01-2019
+#usage           :scrapy crawl etsy_search -a search='3d printed' -o products.csv
+#python_version  :3.6
+#==============================================================================
+
 import scrapy
 
 
@@ -7,5 +16,16 @@ class EtsySearchSpider(scrapy.Spider):
     allowed_domains = ['www.etsy.com']
     start_urls = ['http://www.etsy.com/']
 
+    # Defining the search string
+    def __init__(self, search=None):
+        search_url = "https://www.etsy.com/search?q="
+
+        if search:
+            search_url += search
+
+        self.start_urls = [search_url]
+
+
+    # Parse the first page result and go to the next page
     def parse(self, response):
-        pass
+        scrapy.utils.response.open_in_browser(response)
